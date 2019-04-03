7b904def5b29048000ab28426b2ea282e4441be5
Eleanor Post (RIT Student) <eap9238@ad.rit.edu>
2019-04-02 20:43:31 -0400

    Update Domo.js

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -73,7 +73,7 @@ const getDomosByOwner = (request, response) => {
   const res = response;
     
     // Actually getting the Domos
-  return Domo.DomoModel.findByOwner(req.body.id, (err, docs) => {
+  return Domo.DomoModel.findByOwner(req.body._id, (err, docs) => {
     if (err) {
       console.log(err);
       return res.status(400).json({ error: 'An error occured' });
